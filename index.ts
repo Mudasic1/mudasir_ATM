a82870a62ed91cb213aa82306a5afa8542182279
Mudasir Chandio <mudasirc046@gmail.com>
2024-03-28 16:26:03 -0700

    ATM

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 20000;
+let myPin = 2244;
+
+let pinAns = await inquirer.prompt([{
+   name: "pin",
+   message: "Enter your PIN",
+   type: "number"
+}]);
+
+if(pinAns.pin === myPin){
+    let operation = await inquirer.prompt([{
+        name: "operation",
+        message: "Enter your option",
+        type: "list",
+        choices: ["withdraw", "check balance"]
+    }])
+if(operation.operation === "withdraw"){
+    let amount = await inquirer.prompt([{
+        name: "amount",
+        message: "Enter your amount",
+        type: "number"
+    }])
+    myBalance -= amount.amount
+    console.log("Your remaining balance is "+ myBalance);
+}else if(operation.operation === "check balance"){
+    console.log("Your currently balance is "+ myBalance);
+}
+}else{
+    console.log("Incorrect PIN");
+}
